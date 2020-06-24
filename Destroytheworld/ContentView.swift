33fa74152403a411c391b467033a53332569b940
Abhilash Patel <patelabhilash20@gmail.com>
2020-06-24 10:29:06 +0530

    Initial Commit

---
+++ Destroytheworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Destroytheworld
+//
+//  Created by ABHILASH PATEL on 24/06/20.
+//  Copyright © 2020 ABHILASH PATEL. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DestroytheworldTests/DestroytheworldTests.swift
@@ -0,0 +1,34 @@
+//
+//  DestroytheworldTests.swift
+//  DestroytheworldTests
+//
+//  Created by ABHILASH PATEL on 24/06/20.
+//  Copyright © 2020 ABHILASH PATEL. All rights reserved.
+//
+
+import XCTest
+@testable import Destroytheworld
+
+class DestroytheworldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
